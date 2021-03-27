406a6288f304b433d48959e4788ee37489287727
Caio Cascaes <caio.cascaes@nubank.com.br>
2021-03-27 06:42:22 -0300

    Fixm port

--- src/stuckevergreen/core.clj
+++ src/stuckevergreen/core.clj
@@ -3,11 +3,12 @@
   (:require [stuckevergreen.diplomat.http :as diplomat.http])
   (:gen-class))
 
-(defn handler [_]
+(defn app [_]
   {:status  200
    :headers {"Content-Type" "application/json"}
    :body    (diplomat.http/isstillstuck)})
 
-(defn -main
-  [& _]
-  (run-jetty handler {}))
+(let [port (Integer/parseInt (System/getenv "PORT"))]
+  (run-jetty app {:port port
+                  :path-driver "/app/.chromedriver/bin/chromedriver"
+                  :path-browser "/app/.apt/usr/bin/google-chrome"}))
